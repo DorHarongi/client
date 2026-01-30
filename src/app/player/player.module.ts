37ad7f34f7061b26b65e5be7d0886a3c590ca38b
Dor Harongi <dor_ha@optimove.com>
2026-01-30 10:47:38 +0200

    Fix: add FormsModule to PlayerModule for ngModel

--- src/app/player/player.module.ts
+++ src/app/player/player.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { PlayerPageComponent } from './player-page/player-page.component';
 
 @NgModule({
@@ -7,7 +8,8 @@ import { PlayerPageComponent } from './player-page/player-page.component';
     PlayerPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     PlayerPageComponent
